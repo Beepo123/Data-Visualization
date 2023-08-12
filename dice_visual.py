92c07264513f7b01860bbbc983f8f2a1492ef96a
Samuel Gipit <samuelgipit@gmail.com>
2023-08-12 18:08:47 +0800

    finished roll dice visualization

--- dice_visual.py
+++ dice_visual.py
@@ -1,23 +1,21 @@
 from die import Die
 from plotly.graph_objs import Bar, Layout
 from plotly import offline
+import time
+
+start_time = time.time()
 
 # Create two D6
-die_1 = Die()
-die_2 = Die()
+die_1 = Die(6)
+die_2 = Die(6)
 
 # make some rolls, and store results in a list.
-results = []
-for roll_num in range(1000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for _ in range(1_000_000)]
 
 # count the number of occourances of each side
 # and store it in a list
 max_result = die_1.num_sides + die_2.num_sides
-frequencies = list(range(0,max_result))
-for value in results:
-    frequencies[value-1] += 1
+frequencies = [results.count(value) for value in range(1, max_result+1)]
 
 # Visualize the results.
 x_values = list(range(2, max_result+1))
@@ -25,6 +23,10 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title': 'Result', 'dtick': 1}
 y_axis_config = {'title': 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 dice 1000 times',
+my_layout = Layout(title='Results of rolling D6 and a D10 dice 1000 times',
 xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data': data, 'layout': my_layout}, filename='d6_d6.html')
+offline.plot({'data': data, 'layout': my_layout}, filename='d6_d10.html')
+
+end_time = time.time()
+
+print(f"elapsed time: {end_time-start_time}")
