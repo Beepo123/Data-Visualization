af129e27b0bcb58ba21f1ff0d5c059b77e74b0d0
Samuel Gipit <samuelgipit@gmail.com>
2023-08-16 16:24:41 +0800

    added meta data for earthquake

--- sitka_highs.py
+++ sitka_highs.py
@@ -7,22 +7,31 @@ filename = "data/sitka_weather_2018_simple.csv"
 with open(filename) as file:
     reader = csv.reader(file)
     header_row = next(reader)
-    
+
+
     # Get high temperatyres for this file
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     for row in reader:
-        current_date = datetime.strptime((row[2]), '%Y-%m-%d')
-        high = int(row[5])
-        dates.append(current_date)
-        highs.append(high)
+        try:
+            current_date = datetime.strptime((row[2]), '%Y-%m-%d')
+            high = int(row[5])
+            low = int(row[6])
+        except ValueError:
+            print(f"missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 # Plot the high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, c='red')
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot.
-plt.title("Daily high temperatures, July 2018", fontsize=24)
+plt.title("Daily high and low temperatures - 2018", fontsize=24)
 plt.xlabel('Day', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
